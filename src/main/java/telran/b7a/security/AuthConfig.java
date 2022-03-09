1a007afb55dbe98d770dc02f54b02ea1656d0a40
OgnevOA <ognevoa94@gmail.com>
2022-03-09 14:02:47 +0200

    Updated security service

--- src/main/java/telran/b7a/security/AuthConfig.java
+++ src/main/java/telran/b7a/security/AuthConfig.java
@@ -1,7 +1,6 @@
 package telran.b7a.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,12 +14,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 public class AuthConfig {
 	
 	@Configuration
-	@Order(-1)    
 	public static class CVConfigurerAdapter extends WebSecurityConfigurerAdapter {
+		
 		@Override
 		public void configure(WebSecurity web) {
 			web.ignoring()
-			.antMatchers(HttpMethod.POST, "/cvbank/employee/register**");
+			.antMatchers(HttpMethod.POST, "/cvbank/employee/register**", "/cvbank/employer/register**");
 		}
 		
 		@Override
@@ -30,68 +29,85 @@ public class AuthConfig {
 			http.sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 			http.authorizeRequests()
-				.antMatchers("/cvbank/employee/cv/add**")
+					//==================CV=================
+				.antMatchers("/cvbank/employee/cv/add")
 					.access("hasRole('EMPLOYEE')")
-				.antMatchers("/cvbank/employee/cv/delete/{cvid}**")
+				.antMatchers("/cvbank/employee/cv/delete/{cvid}")
 					.access("@customSecurity.checkCVAuthority(#cvid, authentication.name)")
-				.antMatchers("/cvbank/employee/cv/{cvid}**")
+				.antMatchers("/cvbank/employee/cv/{cvid}")
 					.access("hasRole('EMPLOYER') or hasRole('ADMINISTRATOR') or @customSecurity.checkCVAuthority(#cvid, authentication.name)")
-				.antMatchers("/cvbank/employee/cv/anonymise/{cvid}**")
+				.antMatchers("/cvbank/employee/cv/anonymise/{cvid}")
 					.access("@customSecurity.checkCVAuthority(#cvid, authentication.name)")
+					//==================EMPLOYEE=================
+				.antMatchers(HttpMethod.POST, "/cvbank/employee/login")
+					.authenticated()
+				.antMatchers("/cvbank/employee/{id}")
+					.access("#id == authentication.name")
+					//==================EMPLOYER==================
+				.antMatchers("/cvbank/employer/login")
+					.authenticated()
+				.antMatchers("/cvbank/employer/{companyId}")
+					.access("@customSecurity.checkEmployeeCollectionAuthority(#companyId, authentication.name)")
+				.antMatchers(HttpMethod.PUT, "/cvbank/employer/{companyId}/collection/{collectionName}")
+					.access("@customSecurity.checkEmployeeCollectionAuthority(#companyId, authentication.name)")
+				.antMatchers(HttpMethod.GET, "/cvbank/employer/company/{companyId}")
+					.access("hasRole('EMPLOYER') or hasRole('ADMINISTRATOR')")
 				.anyRequest()
 					.authenticated();
 		}
 	}
 	
-	@Configuration
-	@Order(20)     
-	public static class EmployeeConfigurerAdapter extends WebSecurityConfigurerAdapter {
-		@Override
-		public void configure(WebSecurity web) {
-			web.ignoring()
-			.antMatchers(HttpMethod.POST, "/cvbank/employee/register**");
-		}
-		
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.httpBasic();
-			http.csrf().disable();
-			http.sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-			http.authorizeRequests()
-				.antMatchers("/cvbank/employee/login**")
-					.authenticated()
-				.antMatchers("/cvbank/employee/{id}**")
-					.access("#id == authentication.name")
-				.anyRequest()
-					.authenticated();
-		}
-	}
-	
-	@Configuration
-	@Order(10)
-	public static class EmployerConfigurerAdapter extends WebSecurityConfigurerAdapter {
-		@Override
-		public void configure(WebSecurity web) {
-			web.ignoring()
-			.antMatchers(HttpMethod.POST, "/cvbank/employer/register**");
-		}
-		
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.httpBasic();
-			http.csrf().disable();
-			http.sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-			http.authorizeRequests()
-				.antMatchers("/cvbank/employer/login**")
-					.authenticated()
-				.antMatchers("/cvbank/employer/{id}**")
-					.access("#id == authentication.name")
-				.antMatchers("/cvbank/employer/{id}**")
-					.access("#id == authentication.name")
-				.anyRequest()
-					.authenticated();
-		}
-	}
+//	@Configuration
+//	@Order(1)     
+//	public static class EmployeeConfigurerAdapter extends WebSecurityConfigurerAdapter {
+//		@Override
+//		public void configure(WebSecurity web) {
+//			web.ignoring()
+//			.antMatchers(HttpMethod.POST, "/cvbank/employee/register**");
+//		}
+//		
+//		@Override
+//		protected void configure(HttpSecurity http) throws Exception {
+//			http.httpBasic();
+//			http.csrf().disable();
+//			http.sessionManagement()
+//				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//			http.authorizeRequests()
+//				.antMatchers("/cvbank/employee/login**")
+//					.authenticated()
+//				.antMatchers("/cvbank/employee/{id}**")
+//					.access("#id == authentication.name")
+//				.anyRequest()
+//					.authenticated();
+//		}
+//	}
+//	
+//	@Configuration
+//	@Order(2)
+//	public static class EmployerConfigurerAdapter extends WebSecurityConfigurerAdapter {
+//		@Override
+//		public void configure(WebSecurity web) {
+//			web.ignoring()
+//			.antMatchers(HttpMethod.POST, "/cvbank/employer/register**");
+//		}
+//		
+//		@Override
+//		protected void configure(HttpSecurity http) throws Exception {
+//			http.httpBasic();
+//			http.csrf().disable();
+//			http.sessionManagement()
+//				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//			http.authorizeRequests()
+//				.antMatchers("/cvbank/employer/login**")
+//					.authenticated()
+//				.antMatchers("/cvbank/employer/{id}**")
+//					.access("#id == authentication.name")
+//				.antMatchers("/cvbank/employer/{companyId}/collection**")
+//					.access("@customSecurity.checkEmployeeCollectionAuthority(#companyId, authentication.name)")
+//				.antMatchers("/cvbank/company/{companyId}**")
+//					.access("hasRole('EMPLOYER') or hasRole('ADMINISTRATOR')")
+//				.anyRequest()
+//					.authenticated();
+//		}
+//	}
 }
--- src/main/java/telran/b7a/security/CustomSecurity.java
+++ src/main/java/telran/b7a/security/CustomSecurity.java
@@ -6,14 +6,25 @@ import org.springframework.stereotype.Service;
 import telran.b7a.employeeAccountig.dao.EmployeeAcconutingMongoRepository;
 import telran.b7a.employeeAccountig.dto.exceptions.EmployeeNotFoundException;
 import telran.b7a.employeeAccountig.model.Employee;
+import telran.b7a.employer.dao.EmployerMongoRepository;
+import telran.b7a.employer.exceptions.EmployerNotFoundException;
+import telran.b7a.employer.models.Employer;
 
 @Service("customSecurity")
 public class CustomSecurity {
 	EmployeeAcconutingMongoRepository employeeRepo;
+	EmployerMongoRepository employerRepo;
 
 	@Autowired
-	public CustomSecurity(EmployeeAcconutingMongoRepository employeeRepo) {
+	public CustomSecurity(EmployeeAcconutingMongoRepository employeeRepo, EmployerMongoRepository employerRepo) {
 		this.employeeRepo = employeeRepo;
+		this.employerRepo = employerRepo;
+	}
+
+	public boolean checkEmployeeCollectionAuthority(String companyId, String email) {
+		Employer employerA = employerRepo.findById(companyId).orElseThrow(() -> new EmployerNotFoundException());
+		Employer employerB = employerRepo.findByApplicantInfoEmailIgnoreCase(email);
+		return employerA.equals(employerB);
 	}
 	
 	public boolean checkCVAuthority(String cvid, String name) {
